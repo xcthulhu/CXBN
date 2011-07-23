66b820478e0588a60cfe1f268eec027f04eb55e2
Matthew Wampler-Doty <negacthulhu@gmail.com>
2011-07-23 16:59:11 -0400

    introducing read and write routines for wbw and wbr

--- FPGA/vhdl/common_decs.vhdl
+++ FPGA/vhdl/common_decs.vhdl
@@ -29,9 +29,6 @@ package common_decs is
     ack      : std_logic;               -- Acknowledge
   end record;
 
-  -- Empty read sub-bus
-  constant null_wbr : wbrs := (readdata => (others => '0'), ack => '0');
-
   ---- Common part of the wishbone write system
   type wbws_common is
   record
@@ -47,11 +44,25 @@ package common_decs is
     cycle : std_logic;                  -- Bus cycle in progress
   end record;
 
+  -- Empty bus signals
+  constant null_wbr : wbrs := (readdata => (others => '0'), ack => '0');
   constant null_wbw : wbws := (cycle => '0',
                                c => (address => (others => '0'),
                                      writedata => (others => '0'),
                                      strobe => '0',
                                      writing => '0'));
+
+  
+  -- Create wishbone bus signals
+  function write_wbr (rdata : read_chan) return wbrs;
+  function write_wbw (addrs : addr; wdata : write_chan) return wbws;
+  function read_wbw (addrs : addr) return wbws;
+
+  -- Access protocols for wishbone slave
+  ---- Checks if the master is reading 
+  function master_is_reading(wbw : wbws) return boolean;
+  ---- Checks if the master is writing
+  function master_is_writing(wbw : wbws) return boolean;
 
   -- IMX signals are very similar to Wishbone
   type imx_in is
@@ -62,14 +73,29 @@ package common_decs is
     eb3_n   : std_logic;
   end record;
   
-  -- Access protocols for wishbone slave
-  ---- Checks if the master is reading 
-  function master_is_reading(wbw : wbws) return boolean;
-  ---- Checks if the master is writing
-  function master_is_writing(wbw : wbws) return boolean;
 end package;
 
 package body common_decs is
+
+  function write_wbr (rdata : read_chan) return wbrs is
+  begin return (readdata => rdata, ack => '1');
+  end;
+
+  function write_wbw (addrs : addr; wdata : write_chan) return wbws is
+  begin return (cycle => '1',
+                c => (address => addrs,
+                      writedata => wdata,
+                      strobe => '1',
+                      writing => '1'));
+  end;
+
+  function read_wbw (addrs : addr) return wbws is
+  begin return (cycle => '1',
+                c => (address => addrs,
+                      writedata => (others => '0'),
+                      strobe => '1',
+                      writing => '0'));
+  end;
 
   function master_is_reading(wbw : wbws) return boolean is
   begin return (wbw.c.strobe = '1' and wbw.cycle = '1' and wbw.c.writing = '0');
