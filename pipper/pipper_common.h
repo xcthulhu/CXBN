26342bdb81b84911266909a346a65097f9e2bdab
Matthew Wampler-Doty <negacthulhu@gmail.com>
2011-07-27 21:42:12 -0400

    Universal header for pipper

---
+++ pipper/pipper_common.h
@@ -0,0 +1,8 @@
+#define PIPPER_ACQUIRE      'p'
+#define PIPPER_SYNC_CHAR1   'P'
+#define PIPPER_SYNC_CHAR2   'i'
+#define PIPPER_FLOATS        8
+
+struct pipper {
+	float t0, t1, t2, t3, t4, t5, t6, t7;
+};
